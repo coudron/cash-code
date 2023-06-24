50349e7be5492b6cdd8d1d8181c19ee943dd65c1
Chris Coudron <coudron@gmail.com>
2023-06-24 02:18:54 -0400

    Adding the first batch of quiz stuff

---
+++ chatper2.ts
@@ -0,0 +1,39 @@
+/*
+Chapter 2: Program Structure part 1
+*/
+
+// QUESTION 1
+let ten = 10;
+console.log(ten + ten);
+
+console.log(ten);
+// What is the value of ten after the above statements have run?
+
+// QUESTION 2
+let programState = "stopped";
+console.log(programState);
+
+programState = "running";
+console.log(programState);
+// What does this output
+
+// QUESTION 3
+let total = 100;
+total = total - 25;
+console.log(total);
+// What does this output
+
+// QUESTION 4
+function square(x) {
+  return x * x;
+}
+// What is the value of square(4) after the above code has run?
+// Is x a global variable or a local variable?
+// What is the difference between a global variable and a local variable?
+// What is the difference between a function definition and a function call?
+
+// QUESTION 5
+let theNumber = Number(prompt("Pick a number"));
+// What are the possible values of theNumber after this statement has run?
+
+// Rest of the question for chapter 2 after we do more lessons :)
